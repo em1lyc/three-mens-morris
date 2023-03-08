1ddbbe333fceff0fe8af6f1383f3c253240ac984
dlepighe1 <leorangers77@gmail.com>
2023-03-08 15:25:51 -0600

    New updates on class Game and added a Test class.

--- app/src/main/java/com/example/threemensmorris/Game.java
+++ app/src/main/java/com/example/threemensmorris/Game.java
@@ -1,7 +1,8 @@
 package com.example.threemensmorris;
 
-import android.os.Message;
 import android.os.Handler;
+import android.os.Message;
+import java.util.Scanner;
 
 public class Game {
     // Variables
@@ -14,14 +15,100 @@ public class Game {
     private static Handler mainHandler;
     public static int handlerInit;
 
+
     // Constructor
-    Game(Handler mainHandler) {
-        int handlerInit = 0;
-        initializeGame();
+    Game(){
+        initGameBoard();
     }
 
-    // Initialize the game features and board
-    private void initializeGame() {
+    // Run method
+    public void run(){
+        // Set players
+        Player player1 = new Player();
+        player1.setInformation("Daiki", "X", 1);
+        Player player2 = new Player();
+        player2.setInformation("Ryota", "O", 2);
+
+        players = new Player[2];
+        players[0] = player1;
+        players[1] = player2;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to Three - Men - Morris");
+        System.out.println("-------------------------------");
+        boolean gameOn = true;
+
+        // Place the pieces first
+        while (gameOn) {
+            int turn = 0;
+            while (turn != 2) {
+                display();
+
+                // place the pieces
+                while(true) {
+
+                    System.out.println("\nPlayer " + turn + ": " + players[turn]);
+                    System.out.print("\tEnter the x-coord: ");
+                    int x = input.nextInt();
+
+                    System.out.print("\tEnter the y-coord: ");
+                    int y = input.nextInt();
+
+                    System.out.print("\tEnter the piece: ");
+                    int pieceNum = input.nextInt();
+                    Positions location = players[turn].getPiece(pieceNum);
+                    if (makeMove(players[turn], pieceNum, x, y)) {
+                        // Check
+                        System.out.println("\nDebug");
+                        System.out.println("------");
+                        System.out.println("Player ID: " + players[turn].player_ID);
+                        System.out.println("Player Location: (x = " + location.getPosX() + "; y = " + location.getPosY() + ")");
+                        System.out.println("Piece on board: " + pieceNum);
+                        players[turn].infos();
+
+                        // What's meant to be there
+                        if(checkWinner(players[turn])){
+                            gameOn = false;
+                            break;
+                        }
+                        break;
+                    } else {
+                        System.out.println("\n\nThis is not a valid move");
+                    }
+                }
+
+                System.out.println("\n------------------------------");
+                turn ++;
+            }
+        }
+        System.out.println("End of the Game! There was a winner");
+    }
+
+    // Display game in the terminal
+    public void display(){
+        for (int i = 0; i < 3; i ++){
+            for (int j = 0; j < 3; j ++){
+                if (j == 2) {
+                    System.out.print(gameBoard[i][j]);
+                } else {
+                    System.out.print(gameBoard[i][j] + "  ---  ");
+                }
+            }
+            System.out.println("\n");
+        }
+    }
+
+    /*
+    Method: InitGameBoard
+            Initialize the gameBoard in the format:
+                        0 --- 0 --- 0
+                        0 --- 0 --- 0
+                        0 --- 0 --- 0
+            By default, 0 means that the places are free
+     */
+    public void initGameBoard(){
+        // Set the board
         gameBoard = new int [WIDTH][HEIGHT];
 
         for (int i = 0; i < WIDTH; i ++){
@@ -31,56 +118,54 @@ public class Game {
         }
     }
 
-    // Use threading to pause between moves
-    public static void pauseTime() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            // Do nothing.
+    /*
+    method: isValid
+            Check whether the current position and the next position to
+            see if it's a legal move. The possible moves for one piece
+            are the ones adjacent to that piece.
+     */
+    public boolean isValid(int oldX, int oldY, int newX, int newY){
+        // Check the adjacent positions
+        if (Math.abs(oldX - newX) <= 1 && Math.abs(oldY - newY) <= 1) {
+            // Positions are adjacent
+            return true;
+        } else if (Math.abs(oldX - newX) == 1 && Math.abs(oldY - newY) == 1) {
+            // Positions are diagonal
+            return true;
         }
+
+        // Positions are not adjacent or diagonal
+        return false;
     }
 
     /*
-    Method: get_gameBoard
-            To return the gameBoard we just created
+    Method: makeMove
+            Make the player move on the game board based
+            on their player ID (or Icon in future changes)
      */
-    public static int[][] get_gameBoard() {
-        return gameBoard;
-    }
-
-    /*
-    Method: placePiece
-            For players to place their pieces on the board
-     */
-    public void placePiece(Player player, int pieceNumber, int x, int y) {
-        // Check the count of pieces that the player has on board
+    public boolean makeMove(Player player, int pieceNumber, int x, int y){
+        // Place the pieces on the board first
         if (player.numOfPieces < 3){
-            // Place it in an empty spot
+            // Invalid case
             if (gameBoard[x][y] > 0) {
-                return;
+                return false;
             }
 
             // Set the new position
             gameBoard[x][y] = player.player_ID;
             player.placePiece(pieceNumber, x ,y);
             player.numOfPieces ++;
+            return true;
         }
-    }
 
-    /*
-   Method: makeMove
-           Make the player move on the game board based
-           on their player ID (or Icon in future changes)
-    */
-    public boolean makeMove(Player player, int pieceNumber, int x, int y){
-        // Set the previous place empty
+        // Now, after putting all the pieces, we can make moves around the board
         Positions piece = player.getPiece(pieceNumber);
         int pieceX = piece.getPosX();
         int pieceY = piece.getPosY();
         gameBoard[pieceX][pieceY] = EMPTY; // Free that spot
 
         // Find the move accordingly
-        if (gameBoard[x][y] == 0){
+        if (isValid(pieceX, pieceY, x, y)){
             piece.setPosX(x);
             piece.setPosY(y);
             gameBoard[x][y] = player.player_ID;
@@ -127,7 +212,8 @@ public class Game {
     }
 }
 
-    // Move pieces around the board
+
+// Move pieces around the board
 //    public static void movePiece(Positions oldPos, Positions newPos){
 //        pauseTime();
 //
